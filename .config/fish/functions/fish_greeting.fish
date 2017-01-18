7ae4c5385c63d88e6cdf9ba3a454addcc5de4d03
Ildar Akhmetgaleev <akhilman@gmail.com>
2017-01-18 15:00:48 +0700

    Revert "Moving greeting and fishes title functions to functions/"
    This reverts commit bd6051fd1ee88c8c5ce3ad6feacb366a305011bf.

--- .config/fish/functions/fish_greeting.fish
+++
@@ -1,9 +0,0 @@
-function fish_greeting
-	last -w3 $whami | head -n 3;
-    set have_a_mail (echo quit | mail 2> /dev/null | sed -n 's/.*: \([0-9]* messages*\).*/\1/p')
-    if [ ! -z "$have_a_mail"  ]
-        set color red
-        printf "You have $have_a_mail\n"
-        set color normal
-    end
-end
