2acb3a58fb1ae221d6543a2499131ebdf8421928
Ildar Akhmetgaleev <akhilman@gmail.com>
2017-01-19 20:17:02 +0700

    Ability to disable powerline by env variable

--- .config/fish/conf.d/powerline.fish
+++ .config/fish/conf.d/powerline.fish
@@ -1,10 +1,14 @@
 ###
 # Powerline
+# set FISH_POWERLINE to 1 to enable
 ##
-if status --is-interactive
- 		and [ "$COLORTERM" = "truecolor" ]
-		and [ -e /usr/share/powerline/fish/powerline-setup.fish ]
+if begin
+    status --is-interactive
+    and [ -n "$FISH_POWERLINE" ]
+    and [ "$COLORTERM" = "truecolor" ]
+    and [ -e /usr/share/powerline/fish/powerline-setup.fish ]
+end
 	. /usr/share/powerline/fish/powerline-setup.fish
-	powerline-daemon -q
-	powerline-setup
+    powerline-daemon -q
+    powerline-setup
 end
