02602b5bac38ad8b91cbe838644be605135a76a2
Ildar Akhmetgaleev <akhilman@gmail.com>
2017-01-19 20:11:11 +0700

    Check is there mail command before calling it

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -3,9 +3,12 @@
 ##
 
 function fish_greeting;
-    last -w3 $whami | head -n 3;
-    set have_a_mail (echo quit | mail 2> /dev/null | sed -n 's/.*: \([0-9]* messages*\).*/\1/p')
-    if [ ! -z "$have_a_mail"  ]
+    last -w3 $whami | head -n 3
+    set -l have_a_mail
+    if which mail > /dev/null
+        set have_a_mail (echo quit | mail 2> /dev/null | sed -n 's/.*: \([0-9]* messages*\).*/\1/p')
+    end
+    if [ -n "$have_a_mail"  ]
         set color red
         printf "You have $have_a_mail\n"
         set color normal
