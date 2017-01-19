150e9ca3bc33f070f13a6dcf92fdb2dbeca2f7a8
Ildar Akhmetgaleev <akhilman@gmail.com>
2017-01-19 21:06:08 +0700

    Moving to classic prompt and vcsh in right prompt

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -1,5 +1,12 @@
 function fish_right_prompt
-    set_color grey
-	echo -n (date "+< %F < %R < %A")
-    set_color normal
+
+    set -l vcs_pr
+    set -l date_pr (date "+%F %R %A")
+
+    if functions -q __fish_vcs_prompt
+        set vcs_pr (__fish_vcs_prompt)
+    end
+
+    echo -sn (set_color grey) $date_pr (set_color cyan) $vcs_pr (set_color normal)
+
 end
