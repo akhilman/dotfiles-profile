ba1d79186c61a41ac402783ec9dda71fecfc7a24
Ildar Akhmetgaleev <akhilman@gmail.com>
2017-01-25 16:17:38 +0700

    Fish powerline in Debian

--- .config/fish/conf.d/powerline.fish
+++ .config/fish/conf.d/powerline.fish
@@ -2,13 +2,24 @@
 # Powerline
 # set FISH_POWERLINE to 1 to enable
 ##
+
+set -l setup_script
+
+if [ -e /usr/share/powerline/fish/powerline-setup.fish ]
+    # Fedora
+    set setup_script /usr/share/powerline/fish/powerline-setup.fish
+else if [ -e /usr/share/powerline/bindings/fish/powerline-setup.fish ]
+    # Debian
+    set setup_script /usr/share/powerline/bindings/fish/powerline-setup.fish
+end
+
 if begin
     status --is-interactive
     and [ -n "$FISH_POWERLINE" ]
+    and [ -n "$setup_script" ]
     and [ "$COLORTERM" = "truecolor" ]
-    and [ -e /usr/share/powerline/fish/powerline-setup.fish ]
 end
-	. /usr/share/powerline/fish/powerline-setup.fish
+	. $setup_script
     powerline-daemon -q
     powerline-setup
 end
