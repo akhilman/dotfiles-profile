52a2cc0be684a469bf1dacd15a48539f058186e9
Ildar Akhmetgaleev <akhilman@gmail.com>
2017-01-26 13:58:56 +0700

    Make last logins greeting optional

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,5 +1,11 @@
 function fish_greeting
-	last -w3 $whami | head -n 3
+
+    # last logins
+	if which last > /dev/null
+        last -w3 $whami | head -n 3
+    end
+
+    # mail
     set -l have_a_mail
     if which mail > /dev/null
         set have_a_mail (echo quit | mail 2> /dev/null | sed -n 's/.*: \([0-9]* messages*\).*/\1/p')
@@ -9,4 +15,5 @@ function fish_greeting
         printf "You have $have_a_mail\n"
         set_color normal
     end
+
 end
