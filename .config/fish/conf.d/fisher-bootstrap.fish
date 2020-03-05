def2bb636b588f0a945bb125b917972786164ab8
Ildar Akhmetgaleev <akhilman@gmail.com>
2020-03-05 16:01:42 +0700

    Fix fisher bootstrap

--- .config/fish/conf.d/fisher-bootstrap.fish
+++ .config/fish/conf.d/fisher-bootstrap.fish
@@ -1,7 +1,4 @@
-if not functions -q fisher
-    if ! curl --version > /dev/null
-        return
-    end
+if not functions -q fisher; and curl --version > /dev/null
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
