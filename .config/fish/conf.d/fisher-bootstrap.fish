5df8c423e198cf53ccd7fa9c048724e1b7ec7341
Ildar Akhmetgaleev <akhilman@gmail.com>
2020-03-05 15:54:33 +0700

    Check if curl installed during fisher bootstrap

--- .config/fish/conf.d/fisher-bootstrap.fish
+++ .config/fish/conf.d/fisher-bootstrap.fish
@@ -1,4 +1,7 @@
 if not functions -q fisher
+    if ! curl --version > /dev/null
+        return
+    end
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
