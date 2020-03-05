63102fc76663124561bb06b68a02d3302308db9f
Ildar Akhmetgaleev <akhilman@gmail.com>
2020-03-05 16:13:57 +0700

    Move fish bootstrap to Makefile

--- .config/fish/conf.d/fisher-bootstrap.fish
+++
@@ -1,6 +0,0 @@
-if not functions -q fisher; and curl --version > /dev/null
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-    fish -c fisher add akhilman/fish-prompt-hline
-end
