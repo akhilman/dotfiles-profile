ce989ff603231c93fbdf6b04afaca61d2931e713
Ildar Akhmetgaleev <akhilman@gmail.com>
2020-03-07 04:13:21 +0700

    Redirect stderr of `which` to null
    On OpenSuse `which` prints error message to stderr.

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,13 +1,13 @@
 function fish_greeting
 
     # last logins
-	if which last > /dev/null
+	if which last > /dev/null 2>&1; and [ -r /var/log/wtmp ]
         last -w3
     end
 
     # mail
     set -l have_a_mail
-    if which mail > /dev/null
+    if which mail > /dev/null 2>&1
         set have_a_mail (echo quit | mail 2> /dev/null | sed -n 's/.*: \([0-9]* messages*\).*/\1/p')
     end
     if [ -n "$have_a_mail"  ]
