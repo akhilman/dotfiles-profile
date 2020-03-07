af946cdf34ce53947701509fa9bc2ee802d9f2d4
Ildar Akhmetgaleev <akhilman@gmail.com>
2020-03-07 13:03:25 +0700

    Change color scheme for sudo wrapper

--- .config/fish/functions/sudo.fish
+++ .config/fish/functions/sudo.fish
@@ -1,8 +1,10 @@
 function sudo
+    set_color -o $fish_color_cwd_root
+    echo -n (prompt_pwd)" \$ "
     set_color -r $fish_color_cwd_root
-    echo -n (prompt_pwd) \$ sudo
+    echo -n sudo
     set_color normal
-    set_color $fish_color_cwd_root
+    set_color -o $fish_color_cwd_root
     echo " $argv"
     set_color normal
     command sudo $argv
