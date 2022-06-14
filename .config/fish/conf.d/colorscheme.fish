b6803389c0302df7dc07de9d6d1223a0bdcada0c
Ildar Akhmetgaleev <akhilman@gmail.com>
2022-06-14 19:59:36 +0700

    Apply aya Dark color scheme

--- .config/fish/conf.d/colorscheme.fish
+++ .config/fish/conf.d/colorscheme.fish
@@ -1,12 +1,12 @@
-set fish_color_autosuggestion 969896
+set fish_color_autosuggestion 4D5566
 set fish_color_cancel -r
-set fish_color_command b294bb
-set fish_color_comment f0c674
+set fish_color_command 39BAE6
+set fish_color_comment 626A73
 set fish_color_cwd_root red
 set fish_color_duration blue
-set fish_color_end b294bb
-set fish_color_error cc6666
-set fish_color_escape 00a6b2
+set fish_color_end F29668
+set fish_color_error FF3333
+set fish_color_escape 95E6CB
 set fish_color_hg_added green
 set fish_color_hg_clean green
 set fish_color_hg_copied magenta
@@ -17,17 +17,17 @@ set fish_color_hg_renamed magenta
 set fish_color_hg_unmerged red
 set fish_color_hg_untracked yellow
 set fish_color_history_current --bold
-set fish_color_keyword b294bb
-set fish_color_match --background=brblue
+set fish_color_keyword 39BAE6
+set fish_color_match F07178
 set fish_color_njobs green
-set fish_color_normal normal
-set fish_color_operator 00a6b2
-set fish_color_option 81a2be
-set fish_color_param 81a2be
-set fish_color_quote b5bd68
-set fish_color_redirection 8abeb7
-set fish_color_search_match 'bryellow'  '--background=brblack'
-set fish_color_selection 'white'  '--bold'  '--background=brblack'
+set fish_color_normal B3B1AD
+set fish_color_operator 7c5a16
+set fish_color_option B3B1AD
+set fish_color_param B3B1AD
+set fish_color_quote C2D94C
+set fish_color_redirection FFEE99
+set fish_color_search_match --background=7c5a16
+set fish_color_selection --background=7c5a16
 set fish_color_status '--background=red'  'white'
 set fish_color_time white
 set fish_color_valid_path --underline
@@ -35,4 +35,4 @@ set fish_pager_color_completion normal
 set fish_pager_color_description 'B3A06D'  'yellow'
 set fish_pager_color_prefix 'normal'  '--bold'  '--underline'
 set fish_pager_color_progress 'brwhite'  '--background=cyan'
-set fish_pager_color_selected_background --background=brblack
+set fish_pager_color_selected_background --background=7c5a16
