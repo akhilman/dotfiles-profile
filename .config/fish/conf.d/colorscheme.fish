87eec0e660d64c6822603366f4520e9dd24029aa
Ildar Akhmetgaleev <akhilman@gmail.com>
2022-06-14 11:02:33 +0700

    Add custom colorscheme for fish

---
+++ .config/fish/conf.d/colorscheme.fish
@@ -0,0 +1,43 @@
+set -U fish_color_autosuggestion 707A8C
+set -U fish_color_cancel -r
+set -U fish_color_command 5CCFE6
+set -U fish_color_comment 5C6773
+set -U fish_color_cwd 73D0FF
+set -U fish_color_cwd_root red
+set -U fish_color_duration blue
+set -U fish_color_end F29E74
+set -U fish_color_error FF3333
+set -U fish_color_escape 95E6CB
+set -U fish_color_hg_added green
+set -U fish_color_hg_clean green
+set -U fish_color_hg_copied magenta
+set -U fish_color_hg_deleted red
+set -U fish_color_hg_dirty red
+set -U fish_color_hg_modified yellow
+set -U fish_color_hg_renamed magenta
+set -U fish_color_hg_unmerged red
+set -U fish_color_hg_untracked yellow
+set -U fish_color_history_current --bold
+set -U fish_color_host normal
+set -U fish_color_keyword 5CCFE6
+set -U fish_color_match F28779
+set -U fish_color_njobs green
+set -U fish_color_normal CBCCC6
+set -U fish_color_operator 8c6a26
+set -U fish_color_option CBCCC6
+set -U fish_color_param CBCCC6
+set -U fish_color_quote BAE67E
+set -U fish_color_redirection D4BFFF
+set -U fish_color_search_match --background=8c6a26
+set -U fish_color_selection --background=8c6a26
+set -U fish_color_status '--background'  'red'  'white'
+set -U fish_color_time white
+set -U fish_color_user brgreen
+set -U fish_color_valid_path --underline
+set -U fish_color_vcs yellow
+set -U fish_color_venv magenta
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description 'B3A06D'  'yellow'
+set -U fish_pager_color_prefix 'normal'  '--bold'  '--underline'
+set -U fish_pager_color_progress 'brwhite'  '--background=cyan'
+set -U fish_pager_color_selected_background --background=8c6a26
