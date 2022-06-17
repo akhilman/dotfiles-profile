5433c0c54d8a950e86a8f6518b0ca10db1aab1e8
Ildar Akhmetgaleev <akhilman@gmail.com>
2022-06-17 21:59:12 +0700

    Change prefix of hline prompt variables

--- .config/fish/colorscheme.fish
+++ .config/fish/colorscheme.fish
@@ -11,14 +11,14 @@ set -U fish_color_vcs yellow  # if not generated
 set -U fish_color_venv magenta  # if not generated
 
 # Enable generated colors
-set -U fish_generate_color_cwd 1
-set -U fish_generate_color_hline 1
-set -U fish_generate_color_host 1
-set -U fish_generate_color_user 1
-set -U fish_generate_color_vcs 1
-set -U fish_generate_color_venv 1
+set -U hline_generate_color_cwd 1
+set -U hline_generate_color_hline 1
+set -U hline_generate_color_host 1
+set -U hline_generate_color_user 1
+set -U hline_generate_color_vcs 1
+set -U hline_generate_color_venv 1
 
 # Adjust generated colors
-set -U fish_generated_color_hue_offset 250  # 0 <= H <= 360
-set -U fish_generated_color_saturation 25  # 0 <= S <= 100
-set -U fish_generated_color_value 80  # 0 <= V <= 100
+set -U hline_generated_color_hue_offset 250  # 0 <= H <= 360
+set -U hline_generated_color_saturation 25  # 0 <= S <= 100
+set -U hline_generated_color_value 80  # 0 <= V <= 100
