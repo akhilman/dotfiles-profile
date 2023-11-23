367572e6c843bd21a76847abcd68d3d41faa902a
Ildar Akhmetgaleev <akhilman@gmail.com>
2023-11-23 13:31:34 +0700

    Use long terminal titles

--- .config/fish/functions/fish_title.fish
+++ .config/fish/functions/fish_title.fish
@@ -8,14 +8,8 @@ function fish_title
 
 	if [ $_ = 'fish' ]
 		set title (prompt_pwd)
-		if [ (string length "$title") -gt 15 ]
-			set title ..(string sub --start -13 "$title")
-		end
 	else
 		set title "$argv"
-		if [ (string length "$title") -gt 15 ]
-			set title (string sub --length 13 "$title")...
-		end
 	end
 
 	echo $title
